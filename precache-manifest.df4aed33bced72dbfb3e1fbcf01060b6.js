f0d8d54a1e99010ccda9e5022df86d5165d7f159
quexot <ren67871@gmail.com>
2019-09-18 17:31:21 +0400

    Updates

---
+++ precache-manifest.df4aed33bced72dbfb3e1fbcf01060b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338636ee4678c68934e0940615ce2fa1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3945aa4cdb30bc19a48a",
+    "url": "/Portfolio/static/css/main.6bcadadc.chunk.css"
+  },
+  {
+    "revision": "06c2491c46f9ee8ee463",
+    "url": "/Portfolio/static/js/2.435f18fc.chunk.js"
+  },
+  {
+    "revision": "3945aa4cdb30bc19a48a",
+    "url": "/Portfolio/static/js/main.bf001411.chunk.js"
+  },
+  {
+    "revision": "621176b5c26312058064",
+    "url": "/Portfolio/static/js/runtime~main.c122d43d.js"
+  }
+]);
